5f7e3ce1a44062fa276f7d170cd2305da9680df1
hellen-adeniyi <hellenadeniyi29@gmail.com>
2024-03-06 16:34:29 -0600

    Functions.py
    added doctsrings to the functions in functions.py
    
    debugged the flask boilerplate

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 import csv
+import time
 
 
 with open("users.csv", mode='w', newline='') as f:
-    fieldnames = ['first_name', 'last_name', 'email', 'password', 'phone_number', 'dob', 'city', 'province']
+    fieldnames = ['first_name', 'last_name', 'email', 'password', 'phone_number', 'dob', 'city', 'province', 'play_status', 'last_time_played']
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
 
@@ -16,7 +17,7 @@ def register_user(first_name, last_name, email, password, phone_number, dob, cit
         return first_name, last_name, province, last_time_played
        
 
-register_user(first_name="Hellen", last_name="Adeniyi", email="hellenadeniyi29@gmail.com", password="jord", phone_number="6392954157", dob="05/25/2005", city="saskatoon", province="saskatchewan")
+# register_user(first_name="Hellen", last_name="Adeniyi", email="hellenadeniyi29@gmail.com", password="jord", phone_number="6392954157", dob="05/25/2005", city="saskatoon", province="saskatchewan")
 
 
     
@@ -42,18 +43,41 @@ def confirm_user(email, password):
 
     # Email not found in users.csv
     print("User does not exist !")
-    return 'User does not exist, sign up instead'
+    return "User does not exist, sign up instead"
 
 
 
-confirm_user(email="hellenadeniyi29@gmail.com", password='jord')
+# confirm_user(email="hellenadeniyi29@gmail.com", password='jord')
 
-def check_play_status():
+def check_play_status(email):
     """
-    
-    
+    Purpose: To check the play status of a player, a player can play only once in 24 hours
+        Need to check if they are signed in first, if they are not they need to sign in and if they are
+        the function proceeds to check 
+        if they have played in the last 24 hours.
+        If they have, they cannot play, they  have to wait.
+        If they havent, they can play
+        If they have never played, they can play
+
+    Pre-conditions: email
+    Post-conditions: none
+    return: 1 for the satus of can play and 0 if player cannot play yet 
     """
-    pass
+    last_time_played="never played"
+    play_status = 0
+    password = password
+    if confirm_user(email, password) != "User does not exist, sign up instead":
+        return "You need to Sign Up first"
+        # check if they have never played
+    elif last_time_played =="never played":
+        play_status = 1
+    elif last_time_played < 
+
+
+    # 
+    
+
+    
 
 
 def get_remaining_time():
