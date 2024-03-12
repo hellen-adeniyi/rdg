cbe1b909e103c3dc859b440f20ccb7e07a47c97c
hellen-adeniyi <hellenadeniyi29@gmail.com>
2024-03-12 10:31:28 -0600

    Added check_player_status() which is a function to check if a player can play or not. I created write and read player data for check_player_status()

---
+++ funct.py
@@ -0,0 +1,45 @@
+def sign_in():
+    """
+    Function: 
+    """
+    pass
+
+
+
+# sign up is the same as register
+def sign_up():
+    """
+    """
+    pass
+
+# 
+def check_play_status():
+    """ 
+    """
+    pass
+
+
+def get_remaining_time():
+    """
+    """
+    pass
+
+def get_gift():
+    """
+    """
+    pass
+
+def send_loosing_email():
+    """
+    """
+    pass
+
+def update_last_time_played():
+    """
+    """
+    pass
+
+def database_email():
+    """
+    """
+    pass
